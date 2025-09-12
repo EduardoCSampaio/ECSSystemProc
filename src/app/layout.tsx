624262e9ef0978bad281896957b73de13547ba8f
Eduardo Campos <eduardo.campos.lhamas@gmail.com>
2025-09-12 18:40:51 +0000

    Poderia trocar pelo menos a imagemzinha que aparece no navegador? Talvez

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,11 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
       <head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><path fill=%22%232563eb%22 d=%22M20 10 C14.477 10 10 14.477 10 20 L10 80 C10 85.523 14.477 90 20 90 L80 90 C85.523 90 90 85.523 90 80 L90 40 L60 10 L20 10 Z%22/><path fill=%22%2393c5fd%22 d=%22M90 40 L60 40 L60 10 L90 40 Z%22/></svg>"
+          type="image/svg+xml"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
