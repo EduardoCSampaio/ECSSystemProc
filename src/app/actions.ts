8fa115ea9a7ad4dd9e2e20322a251580d6a3e4c6
Eduardo Campos <eduardo.campos.lhamas@gmail.com>
2025-09-12 20:02:00 +0000

    Estou recebendo esse erro ao subir, parece ser a formatação de como está

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -238,8 +238,8 @@ function processV8Digital(data: any[]): any[] {
         // Map and transform data based on V8Digital rules
         newRow['NUM_BANCO'] = 17;
         newRow['NOM_BANCO'] = 'V8DIGITAL';
-        newRow['NUM_PROPOSTA'] = String(sourceRow['NUM_PROPOSTA'] || '');
-        newRow['NUM_CONTRATO'] = String(sourceRow['NUM_CONTRATO'] || '');
+        newRow['NUM_PROPOSTA'] = Number(sourceRow['NUM_PROPOSTA']) || 0;
+        newRow['NUM_CONTRATO'] = Number(sourceRow['NUM_CONTRATO']) || 0;
         newRow['DSC_TIPO_PROPOSTA_EMPRESTIMO'] = sourceRow['DSC_TIPO_PROPOSTA_EMPRESTIMO'] === 'Margem Livre (Novo)' ? 'NOVO' : sourceRow['DSC_TIPO_PROPOSTA_EMPRESTIMO'];
         newRow['COD_PRODUTO'] = '';
         newRow['DSC_PRODUTO'] = sourceRow['DSC_PRODUTO'] || '';
@@ -318,8 +318,8 @@ function processUnno(data: any[]): any[] {
         // Map and transform data based on UNNO rules
         newRow['NUM_BANCO'] = 9209;
         newRow['NOM_BANCO'] = 'UNNO';
-        newRow['NUM_PROPOSTA'] = String(sourceRow['CCB'] || '');
-        newRow['NUM_CONTRATO'] = String(sourceRow['CCB'] || '');
+        newRow['NUM_PROPOSTA'] = Number(sourceRow['CCB']) || 0;
+        newRow['NUM_CONTRATO'] = Number(sourceRow['CCB']) || 0;
         newRow['DSC_TIPO_PROPOSTA_EMPRESTIMO'] = 'NOVO';
         newRow['COD_PRODUTO'] = '';
         newRow['DSC_PRODUTO'] = sourceRow['Tabela'] || '';
